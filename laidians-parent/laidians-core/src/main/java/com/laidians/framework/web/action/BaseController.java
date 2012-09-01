1e571c53fb26f49e31e139c72f3d154b00388ad7
wxylon@gmail.com <wxylon@gmail.com@e0829cac-269c-9ca1-3040-2c2d6666ffcb>
2012-09-01 10:40:27 +0000

    框架调试成功

---
+++ laidians-parent/laidians-core/src/main/java/com/laidians/framework/web/action/BaseController.java
@@ -0,0 +1,13 @@
+/**
+* Copyright(c) 2002-2012, wxylon@gmail.com  All Rights Reserved
+*/
+
+package com.laidians.framework.web.action;
+
+/**
+ * @author wxylon@gmail.com
+ * @date 2012-9-1
+ */
+public class BaseController {
+}
+
---
+++ laidians-parent/laidians-web/src/main/java/com/laidians/blog/web/action/DynamicController.java
@@ -0,0 +1,39 @@
+package com.laidians.blog.web.action;
+
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.laidians.framework.web.action.BaseController;
+
+/**
+ * 动态控制器入口
+ * @author wangx
+ */
+@Controller
+public class DynamicController extends BaseController {
+	
+	/**
+	 * 根地址访问
+	 * @author wangx
+	 */
+	@RequestMapping(value = "/", method = {RequestMethod.GET, RequestMethod.POST})
+	public String index(HttpServletRequest request, ModelMap model) {
+		model.put("stylesheet", "12121.css");
+		model.put("title", "12121.css");
+		return "pages/hello";
+	}
+	
+	/**
+	 * /index.html 地址访问
+	 * @author wangx
+	 */
+	@RequestMapping(value = "/index.html", method = {RequestMethod.GET, RequestMethod.POST})
+	public String indexForWeblogic(HttpServletRequest request, ModelMap model) {
+		return index(request, model);
+	}
+}
